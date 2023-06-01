786e1aac2bbf0b0304a0e41bd27b29771c784ca6
Roman Amanov <roman.amanov@avotech.uz>
2023-06-01 22:53:48 +0400

    small fix

--- task1/src/main/java/ru/otus/task1/controller/BaseController.java
+++ task1/src/main/java/ru/otus/task1/controller/BaseController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,17 @@ public class BaseController {
         return ResponseEntity.ok(product);
     }
 
+    @PostMapping("/update")
+    public ResponseEntity<String> productsUpdate(@RequestParam String product) {
+        log.debug("ProductsApiDelegate.productsCreateNew: {}", product);
+        return ResponseEntity.ok(product);
+    }
+
     @GetMapping("/get/{username}")
     public ResponseEntity<String> checkProducts(@PathVariable String username) {
         log.debug("ProductsApiDelegate.checkProducts: {}", username);
         return ResponseEntity.ok(username);
     }
+
 
 }
